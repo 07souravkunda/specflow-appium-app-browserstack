ae876114ea6a911e43dd2ea224dea025c6911317
Sourav Kunda <50537259+07souravkunda@users.noreply.github.com>
2024-09-25 14:35:23 +0530

    add: inital appium

---
+++ android/resources/src/BrowserStackSpecFlowTest.cs
@@ -0,0 +1,51 @@
+﻿using TechTalk.SpecFlow;
+using log4net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium;
+
+namespace SpecFlowBrowserStack
+{
+	[Binding]
+	public class BrowserStackSpecFlowTest
+	{
+		private FeatureContext _featureContext;
+		private ScenarioContext _scenarioContext;
+
+		public static ThreadLocal<IWebDriver> ThreadLocalDriver = new ThreadLocal<IWebDriver>();
+		private static readonly ILog log = LogManager.GetLogger(typeof(BrowserStackSpecFlowTest));
+
+		public BrowserStackSpecFlowTest(FeatureContext featureContext, ScenarioContext scenarioContext)
+		{
+			_featureContext = featureContext;
+			_scenarioContext = scenarioContext;
+		}
+
+
+		[BeforeScenario]
+		public static void Initialize(ScenarioContext scenarioContext)
+		{
+			AppiumOptions appiumOptions = new AppiumOptions();
+			appiumOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Samsung Galaxy S20");
+			appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+			appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "10");
+			ThreadLocalDriver.Value = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), appiumOptions);
+		}
+
+
+		[AfterScenario]
+		public static void TearDown(ScenarioContext scenarioContext)
+		{
+			Shutdown();
+		}
+
+		protected static void Shutdown()
+		{
+			if (ThreadLocalDriver.IsValueCreated)
+			{
+				ThreadLocalDriver.Value?.Quit();
+			}
+		}
+	}
+}
---
+++ android/resources/src/SampleTest.cs
@@ -0,0 +1,64 @@
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using SpecFlowBrowserStack;
+using TechTalk.SpecFlow;
+
+
+
+namespace SpecFlowBasics.StepDefinitions
+{
+    [Binding]
+
+    public class SampleTest
+    {
+      private readonly IWebDriver? _driver;
+			private string? productOnPageText;
+			private string? productOnCartText;
+			private bool? cartOpened;
+			readonly WebDriverWait wait;
+			public SampleTest()
+			{
+				_driver = BrowserStackSpecFlowTest.ThreadLocalDriver.Value;
+				wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+			}
+
+      [Given(@"I open the App")]
+			public void GivenINavigatedTowebsite()
+			{
+			}
+
+			[Then(@"I click on search wikipedia")]
+			public void ThenIClickOnSearchWikipedia()
+			{
+				Thread.Sleep(5000);
+				AndroidElement searchElement = (AndroidElement)new WebDriverWait(_driver, TimeSpan.FromSeconds(30)).Until(ExpectedConditions.ElementToBeClickable(MobileBy.AccessibilityId("Search Wikipedia")));
+      	searchElement.Click();
+			}
+
+			[Then(@"I enter '(.*)'")]
+			public void ThenEnter(string input)
+			{
+				AndroidElement insertTextElement = (AndroidElement)new WebDriverWait(_driver, TimeSpan.FromSeconds(30)).Until(ExpectedConditions.ElementToBeClickable(By.Id("org.wikipedia.alpha:id/search_src_text")));
+				insertTextElement.SendKeys("BrowserStack");
+			}
+			[When(@"I check if products are opened")]
+			public void WhenICheckIfCartIsOpened()
+			{
+
+				cartOpened = _driver?.FindElement(By.ClassName("android.widget.TextView")).Displayed;
+				Assert.IsTrue(cartOpened);
+			}
+			[Then(@"I should see same product in cart")]
+			public void ThenIShouldSeeSameProductInCart()
+			{
+				ReadOnlyCollection<IWebElement>? allProductsName = _driver?.FindElements(By.ClassName("android.widget.TextView"));
+      	Assert.IsTrue(allProductsName?.Count > 0);
+			}
+
+    }
+}
